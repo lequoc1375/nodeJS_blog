d043153bd516c806ddf0a3b8ee8a04b3a8ace103
Admin <tanhocvovinam@gmail.com>
2025-02-14 14:42:34 +0700

    nodeJS blog

---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars'); 
+const route = require('./routes')
+const app = express();
+const port = 3000;
+
+// Logger
+app.use(morgan('combined'));
+
+// Template Handlebars engine
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname,'resources','views'));  // Đảm bảo bạn có thư mục `views`
+
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Routes init
+route(app);
+
+// Listen
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
