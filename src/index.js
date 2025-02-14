e840335589fbbc522c779d4eb24f9cb0e8c5a6b7
Admin <tanhocvovinam@gmail.com>
2025-02-14 15:44:03 +0700

    test format

--- src/index.js
+++ src/index.js
@@ -2,26 +2,26 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const { engine } = require('express-handlebars')
-const route = require('./routes');
+const route = require('./routes')
 const app = express()
 const port = 3000
 
 // Logger
-    app.use(morgan('combined'));
+app.use(morgan('combined'))
 
 // Template Handlebars engine
-                    app.engine('handlebars', engine());
-app.set('view engine', 'handlebars')
-                   app.set('views', path.join(__dirname, 'resources', 'views')) // Đảm bảo bạn có thư mục `views`
+    app.engine('handlebars', engine())
+     app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'resources', 'views')) // Đảm bảo bạn có thư mục `views`
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }))
-                      app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+              app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-// Routes init
+                 // Routes init
 route(app)
 
 // Listen
-app.listen(port, () =>
+                   app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 )
