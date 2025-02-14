526ca421a2d5c65fe9525352443853c283a244ff
Admin <tanhocvovinam@gmail.com>
2025-02-14 15:37:42 +0700

    format code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-
 const path = require('path')
-const express = require('express');
-const morgan = require('morgan');
-const { engine } = require('express-handlebars'); 
-const route = require('./routes')
-const app = express();
-const port = 3000;
+const express = require('express')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+const route = require('./routes');
+const app = express()
+const port = 3000
 
 // Logger
-app.use(morgan('combined'));
+    app.use(morgan('combined'));
 
 // Template Handlebars engine
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname,'resources','views'));  // Đảm bảo bạn có thư mục `views`
+                    app.engine('handlebars', engine());
+app.set('view engine', 'handlebars')
+                   app.set('views', path.join(__dirname, 'resources', 'views')) // Đảm bảo bạn có thư mục `views`
 
-
-app.use(express.static(path.join(__dirname,'public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }))
+                      app.use(express.json())
 
 // Routes init
-route(app);
+route(app)
 
 // Listen
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+)
